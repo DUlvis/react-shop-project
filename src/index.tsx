30607b7e0b4117033b3b3fa3d0f64d76eb179a57
Denis <ITUlvis@gmail.com>
2022-12-20 14:24:10 +0200

    add arrow function component

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-function App() {
+const App = () => {
     return (
         <div>
             <h1>Hello React</h1>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Impedit aperiam vero dolore sapiente eum harum et blanditiis ab
+                cum quisquam, sunt mollitia vel optio explicabo numquam error
+                ullam maiores. Sunt?
+            </p>
             <p>
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                 Impedit aperiam vero dolore sapiente eum harum et blanditiis ab
