46af4f3403cb8679fb2ded459b526721a51c9377
Denis <ITUlvis@gmail.com>
2022-12-20 14:33:24 +0200

    connection App component (2-components)

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+const Title = () => {
+    return <h1>Hello React</h1>
+}
+const Content = () => {
+    return (
+        <div>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Impedit aperiam vero dolore sapiente eum harum et blanditiis ab
+                cum quisquam, sunt mollitia vel optio explicabo numquam error
+                ullam maiores. Sunt?
+            </p>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Impedit aperiam vero dolore sapiente eum harum et blanditiis ab
+                cum quisquam, sunt mollitia vel optio explicabo numquam error
+                ullam maiores. Sunt?
+            </p>
+        </div>
+    )
+}
 const App = () => {
     return (
         <div>
-            <h1>Hello React</h1>
-            <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Impedit aperiam vero dolore sapiente eum harum et blanditiis ab
-                cum quisquam, sunt mollitia vel optio explicabo numquam error
-                ullam maiores. Sunt?
-            </p>
-            <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Impedit aperiam vero dolore sapiente eum harum et blanditiis ab
-                cum quisquam, sunt mollitia vel optio explicabo numquam error
-                ullam maiores. Sunt?
-            </p>
+            <Title />
+            <Content />
         </div>
     )
 }
