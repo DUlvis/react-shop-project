f80640e38fce1dc33bcb3c49df7c1d3a80bd10cb
Denis <ITUlvis@gmail.com>
2022-12-26 00:49:22 +0200

    add ProductsListItem

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,17 @@
+import { Typography } from '@mui/material'
+import ProductsList from 'components/Products/ProductsList'
+
 type Props = {}
 
 const Home = (props: Props) => {
-    return <div>Home</div>
+    return (
+        <>
+            <Typography variant="h4" component="h1" align="center">
+                List of Products
+            </Typography>
+            <ProductsList />
+        </>
+    )
 }
 
 export default Home
