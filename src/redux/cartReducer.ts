711c6c5c61b8bbc9a866e50c6acc67a1c7d01f67
Denis <ITUlvis@gmail.com>
2023-01-20 01:24:48 +0200

    add addProductToCart , removeProductFromCart and changeProductQuantity from reduce

--- src/redux/cartReducer.ts
+++ src/redux/cartReducer.ts
@@ -1,18 +1,35 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit'
+import { omit } from 'lodash'
 
 type ProductsInCart = {
     [id: number]: number
 }
 
-export const initialState:ProductsInCart = {
+export const initialState: ProductsInCart = {
     1: 3,
     2: 3,
 }
 
 export const cartSlice = createSlice({
-    name:"productsInCart",
+    name: 'productsInCart',
     initialState,
-    reducers:{}
+    reducers: {
+        addProductToCart: (state, action) => ({
+            ...state,
+            [action.payload.id]:
+                (state[action.payload.id] || 0) + action.payload.count,
+        }),
+        removeProductFromCart: (state, action) => omit(state, action.payload),
+        changeProductQuantity: (state, action) => ({
+            ...state,
+            [action.payload.id]: action.payload.count,
+        }),
+    },
 })
 
+export const {
+    addProductToCart,
+    removeProductFromCart,
+    changeProductQuantity,
+} = cartSlice.actions
 export default cartSlice.reducer
