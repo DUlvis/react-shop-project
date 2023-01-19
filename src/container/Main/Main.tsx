e6a95b8357207a431379dc445f0f6a33ec85c05c
Denis <ITUlvis@gmail.com>
2023-01-20 01:31:12 +0200

    add cartReducer

--- src/container/Main/Main.tsx
+++ src/container/Main/Main.tsx
@@ -6,38 +6,15 @@ import PaymentPage from 'pages/PaymentPage/PaymentPage'
 import ShippingPage from 'pages/ShippingPage/ShippingPage'
 import { Route, Routes } from 'react-router-dom'
 
-type Props = {
-    addProductToCart: (id: number, count: number) => void
-    productsInCart: { [id: number]: number }
-    removeProductFromCart: (id: number) => void
-    changeProductQuantity: (id: number, count: number) => void
-}
-
-const Main = ({
-    addProductToCart,
-    productsInCart,
-    removeProductFromCart,
-    changeProductQuantity,
-}: Props) => {
+const Main = () => {
     return (
         <Container maxWidth="lg" sx={{ padding: '50px 0' }} component="main">
             <Routes>
-                <Route
-                    path="/"
-                    element={<Home addProductToCart={addProductToCart} />}
-                />
+                <Route path="/" element={<Home />} />
                 <Route path="about" element={<AboutPage />} />
                 <Route path="shipping" element={<ShippingPage />} />
                 <Route path="payment" element={<PaymentPage />} />
-                <Route
-                    path="cart"
-                    element={
-                        <CartPage
-                            removeProductFromCart={removeProductFromCart}
-                            changeProductQuantity={changeProductQuantity}
-                        />
-                    }
-                />
+                <Route path="cart" element={<CartPage />} />
             </Routes>
         </Container>
     )
